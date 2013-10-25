3ad64c06f917efdaa94ad5debc23941f4d95105a
Michael Still <mikal@stillhq.com>
2013-10-25 14:31:51 +1100

    Return table name not model object name.

--- fuzzy_happiness/attributes.py
+++ fuzzy_happiness/attributes.py
@@ -36,10 +36,17 @@ def load_configuration():
         if not issubclass(obj, models.NovaBase):
             continue
 
-        if not hasattr(obj, '__confidential__'):
+        attrs_missing = []
+        for required_attr in ['__tablename__', '__confidential__']:
+            if not hasattr(obj, required_attr):
+                attrs_missing.append(required_attr)
+
+        if attrs_missing:
+            print ('Required attributes %s missing from %s'
+                   %(', '.join(attrs_missing), name))
             continue
 
-        configs[name] = obj.__confidential__
+        configs[obj.__tablename__] = obj.__confidential__
 
     return configs
 
